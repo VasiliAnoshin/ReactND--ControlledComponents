4f64364dd529caf6dedc9dd9be675d2a31913dca
Vasili <anoshin45@gmail.com>
2018-12-17 21:58:22 -0800

    change items.js

--- src/Items.js
+++ src/Items.js
@@ -5,16 +5,16 @@ class Items extends Component{
 	render(){
 		return(
 			<div>
-          <p className="items">Items</p>
-          <ol className="item-list">
-            {this.props.state.items.map((item, index) =>
-            <li key={index}>{item}</li>)}
-        	</ol>
-      </div>
-    )}
+				<p className="items">Items</p>
+				<ol className="item-list">
+					{this.props.state.items.map((item, index) =>
+					<li key={index}>{item}</li>)}
+				</ol>
+			</div>
+	)}
 } 
 Items.propTypes = {
-  state: PropTypes.object.isRequired,
+	state: PropTypes.object.isRequired,
 };
 
 
